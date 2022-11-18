2367292a93091f011fc52066d9a20b09f62cadd4
doug <byfar357@gmail.com>
2022-11-18 15:49:00 -0500

    added read button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* DOM items */
 
-let MainContainer = document.querySelector(".container-m");
+let mainContainer = document.querySelector(".container-m");
 
 let bookButton = document.querySelector(".book-btn");
 
@@ -14,12 +14,13 @@ let authorInfo = document.querySelector("#author").value;
 
 let pageNumberInfo = document.querySelector("#page-number").value;
 
-let populateBook = document.querySelector(".populate-book");
+let readInfo = document.querySelector("#read").checked;
 
-console.log(titleInfo);
+let populateBook = document.querySelector(".populate-book");
 
 /* main Array */
 let myLibrary = [];
+
 
 function book (title, author, pages, read){
     this.title = title
@@ -28,22 +29,15 @@ function book (title, author, pages, read){
     this.read = read
 }
 
-book.prototype.info = function(){
-    return `<p>${this.title}<p> `+ `<p>${this.author}<p>` + `<p>${this.pages} pages<p>` + `<p>${this.read}<p>`
-}
 
-let HP = new book ("Harry Potter", "J.K. Rowling.", "202", "I have read this" );
 
-let CS = new book ("javascript tutorial", "John Smith", "300", "i have read this book");
+let HP = new book ("Harry Potter", "J.K. Rowling.", "202", "Read" );
+
+let CS = new book ("javascript tutorial", "John Smith", "300", "Not Read");
 
 myLibrary.push(HP);
 
 myLibrary.push(CS);
-
-console.log(myLibrary)
-
-console.log(HP.info());
-
 
 /* <----------------------eventListeners---------------------------->*/
 
@@ -61,26 +55,112 @@ background.addEventListener("click", function() {
 
 /* event listen that will add info from form to array and then run addBook function*/
 populateBook.addEventListener("click", function(){
+    //form values
     titleInfo = document.querySelector("#title").value;
     authorInfo = document.querySelector("#author").value;
     pageNumberInfo = document.querySelector("#page-number").value;
-    let newInput = new book (titleInfo, authorInfo, pageNumberInfo, "Read it");
+    readInfo = document.querySelector("#read").checked;
+    function readOutPut(){
+    if (readInfo == true){
+        return "Read"
+    }
+    else {
+        return "Not Read"
+    }
+    }
+    //create book, push to array, and run render book function
+    let newInput = new book (titleInfo, authorInfo, pageNumberInfo, readOutPut());
     myLibrary.push(newInput);
-    addBook();
     event.preventDefault();
+    renderBook();
 })
 
 
-
-
 /* function that will first clear innerHTML of main container (so that books arent duplicated) then add all array items to the main container*/
-function addBook (){
-    MainContainer.innerHTML = "";
+function renderBook (){
+    mainContainer.innerHTML = "";
     for(let i = 0; i < myLibrary.length; i++){
+        //book div
         let bookContain = document.createElement("div");
-        bookContain.innerHTML = myLibrary[i].info();
-        MainContainer.appendChild(bookContain);
+        bookContain.dataset.index = [i];
+        mainContainer.appendChild(bookContain);
+
+        //title
+        let titlePara = document.createElement("h2");
+        titlePara.innerText = myLibrary[i].title;
+        bookContain.appendChild(titlePara);
+
+        //author
+        let authorPara = document.createElement("i");
+        authorPara.innerText = myLibrary[i].author;
+        bookContain.appendChild(authorPara);
+
+        //pages
+        let pagesPara = document.createElement("p");
+        pagesPara.innerText = `${myLibrary[i].pages} pages`
+        bookContain.appendChild(pagesPara);
+
+        //read btn
+        let readButton = document.createElement('button');
+        readButton.classList.add("btn");
+        if (myLibrary[i].read == "Read") {
+        readButton.innerText = myLibrary[i].read;
+        readButton.classList.remove("green-btn");
+        readButton.classList.add("red-btn");
+        bookContain.appendChild(readButton);
+        }
+        else {
+        readButton.innerText = myLibrary[i].read;
+        readButton.classList.remove("red-btn");
+        readButton.classList.add("green-btn");
+        bookContain.appendChild(readButton);
+        }
+        //delete button
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn");
+        deleteButton.classList.add("delete-btn");
+        deleteButton.dataset.index = [i];
+        deleteButton.innerText = "Delete Book"
+        bookContain.appendChild(deleteButton);
+        
+        //event listeners
+        deleteButton.addEventListener("click", function(){
+            myLibrary.splice(i, 1);
+            renderBook();
+            
+        })
+        console.log(myLibrary[i].read);
+
+        readButton.addEventListener("click", function(){
+            if (myLibrary[i].read == "Read"){
+                myLibrary[i].read = "Not Read"
+                readButton = document.createElement('button');
+                readButton.innerText = myLibrary[i].read;
+                bookContain.appendChild(readButton);
+                renderBook();
+
+            }
+            else{
+                myLibrary[i].read = "Read"
+                readButton = document.createElement('button');
+                readButton.innerText = myLibrary[i].read;
+                bookContain.appendChild(readButton);
+                renderBook();
+            }
+        })
+            }
+            
+
     }
+
+
+renderBook();
+
+function theClasses(){
+    return "red-btn", "green btn"
 }
 
-addBook();
+console.log(myLibrary);
+
+// use the i !!!!!
+// need to make it so all book do not have to be refreshed each time set restriction for adding books to array if the title matches (use inlcudes or some method)
